de8594b68ffdf2032f14d92185bc5ab3f7104f83
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-09-25 12:07:28 +0200

    Adding package exceptions

---
+++ src/main/java/fr/pederobien/minecraftcoordinates/commands/exceptions/GpsEntryAlreadyRegisteredException.java
@@ -0,0 +1,21 @@
+package fr.pederobien.minecraftcoordinates.commands.exceptions;
+
+import fr.pederobien.minecraftcoordinates.commands.gps.GpsEntry;
+import fr.pederobien.minecraftgameplateform.exceptions.SimpleMessageException;
+
+public class GpsEntryAlreadyRegisteredException extends SimpleMessageException {
+	private static final long serialVersionUID = 1L;
+	private GpsEntry gps;
+
+	public GpsEntryAlreadyRegisteredException(GpsEntry gps) {
+		super("The gps " + gps.getDestinationName() + " is already registered");
+		this.gps = gps;
+	}
+
+	/**
+	 * @return The already registered gps entry.
+	 */
+	public GpsEntry getGps() {
+		return gps;
+	}
+}
---
+++ src/main/java/fr/pederobien/minecraftcoordinates/commands/exceptions/GpsEntryNotRegisteredException.java
@@ -0,0 +1,20 @@
+package fr.pederobien.minecraftcoordinates.commands.exceptions;
+
+import fr.pederobien.minecraftgameplateform.exceptions.SimpleMessageException;
+
+public class GpsEntryNotRegisteredException extends SimpleMessageException {
+	private static final long serialVersionUID = 1L;
+	private String destinationName;
+
+	public GpsEntryNotRegisteredException(String destinationName) {
+		super("The gps " + destinationName + " is not registered");
+		this.destinationName = destinationName;
+	}
+
+	/**
+	 * @return The not registered gps entry name.
+	 */
+	public String getDestinationName() {
+		return destinationName;
+	}
+}
